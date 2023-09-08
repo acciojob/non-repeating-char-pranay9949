aeb4f7e74b21062cd30ff219f49be933078ec2c2
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-08 15:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ function firstNonRepeatedChar(str) {
 	a.sort();
 	let b=a.join("");
 	let i=1;
-	let s="";
+	let s=null;
+	if(b.charAt(0)!=b.charAt(0+1)){
+        s=b.charAt(0);
+		return s
+	}
+	
 	while(i<b.length-1){
+		
 		if(b.charAt(i-1)!=b.charAt(i) && b.charAt(i+1)!=b.charAt(i)){
 			s=b.charAt(i);
+			return s;
 			break;
 		} 
+		
 		i++;
+	
+	}
+	if(b.charAt(b.lenght-1)!=b.charAt(b.length-2)){
+        s=b.charAt(b.length-1);
+		return s
 	}
 	return s;
 	 
