7904d66bd06bac7882bad84f66635a1d73abcbac
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-08 15:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+    let a =str.split("");
+	a.sort();
+	let b=a.join("");
+	let i=1;
+	let s="";
+	while(i<b.length-1){
+		if(b.charAt(i-1)!=b.charAt(i) && b.charAt(i+1)!=b.charAt(i)){
+			s=b.charAt(i);
+			break;
+		} 
+		i++;
+	}
+	return s;
+	 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
